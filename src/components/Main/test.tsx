684e269695d01b56ef96d07177857ffe4327bf6b
Diogenes <diogensgreen@gmail.com>
2021-01-20 14:28:49 -0300

    Update test.tsx

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,7 +5,6 @@ import Main from '.'
 describe('<Main/>', () => {
   it('should render the heading', () => {
     // const { container } = render(<Main />)
-
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
